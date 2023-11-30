219f1a09090229f74329a8f21b91daa8b87f651d
Nicolas Montiel <nicolasmontielf@gmail.com>
2023-11-30 17:11:57 -0300

    First version of leaderboard page

---
+++ src/components/leaderboard/LeaderBoardTable.vue
@@ -0,0 +1,40 @@
+<script setup>
+    import { reactive } from 'vue'
+
+    const props = defineProps({
+        isHeader: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    })
+
+    const classes = reactive({
+        'text-sm grid grid-cols-8 py-5 px-5 border-b-slate-100 border': true,
+        'font-semibold bg-blue-100': props.isHeader,
+    })
+</script>
+
+<template>
+    <div :class="classes">
+        <div class="col-span-4">
+            <slot name="teamName"></slot>
+        </div>
+        
+        <div class="col-span-1">
+            <slot name="mp"></slot>
+        </div>
+
+        <div class="col-span-1">
+            <slot name="gf"></slot>
+        </div>
+
+        <div class="col-span-1">
+            <slot name="ga"></slot>
+        </div>
+
+        <div class="col-span-1">
+            <slot name="points"></slot>
+        </div>
+    </div>
+</template>
--- src/views/LeaderboardPage.vue
+++ src/views/LeaderboardPage.vue
@@ -1,3 +1,53 @@
+<script setup>
+    import LeaderBoardTable from '../components/leaderboard/LeaderBoardTable';
+    import TeamWithFlag from '../components/team/TeamWithFlag'
+</script>
+
 <template>
-    <h3>Leaderboard page</h3>
+    <div class="w-full">
+        <div class="mb-5">
+            <h1 class="text-center text-2xl font-bold">League Standings</h1>
+        </div>
+
+        <div id="leaderboard-table-container">
+            <!-- Table Header -->
+            <LeaderBoardTable is-header>
+                <template #teamName>
+                    <p>Team</p>
+                </template>
+                <template #mp>
+                    <p>MP</p>
+                </template>
+                <template #gf>
+                    <p>GF</p>
+                </template>
+                <template #ga>
+                    <p>GA</p>
+                </template>
+                <template #points>
+                    <p>Points</p>
+                </template>
+            </LeaderBoardTable>
+
+            <!-- Table Body -->
+            <LeaderBoardTable >
+                <template #teamName>
+                    <TeamWithFlag team="Brazil" reverse />
+                </template>
+                <template #mp>
+                    <p>3</p>
+                </template>
+                <template #gf>
+                    <p>8</p>
+                </template>
+                <template #ga>
+                    <p>4</p>
+                </template>
+                <template #points>
+                    <p>7</p>
+                </template>
+            </LeaderBoardTable>
+
+        </div>
+    </div>
 </template>
