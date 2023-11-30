74baf258736406a90b8ca81dce565f2947806517
Nicolas Montiel <nicolasmontielf@gmail.com>
2023-11-30 14:27:52 -0300

    Initial implementation of router

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LeaderboardPage from '../views/LeaderboardPage.vue'
+import SchedulePage from '../views/SchedulePage.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: SchedulePage },
+        { path: '/schedule ', component: SchedulePage },
+        { path: '/leaderboard ', component: LeaderboardPage },
+    ]
+})
+
+export default router;
