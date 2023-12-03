03ea7e72456d31b5a01d2cb0c0c6f66ba526e003
Nicolas Montiel <nicolasmontielf@gmail.com>
2023-12-03 02:10:20 -0300

    Tests for LeaderboardService

--- tests/leaderboard.test.js
+++ tests/leaderboard.test.js
@@ -5,46 +5,93 @@
  *
  */
 
-require('jest-fetch-mock').enableMocks();
-fetchMock.dontMock();
+import { expect } from "@jest/globals";
+import LeaderboardService from "../src/services/LeaderboardService";
+import MATCHES from './matches.json'
 
-import LeagueService from "../src/services/LeagueService";
+describe("LeaderboardService", () => {
+    test('Tiebreaker by head-to-head points with 2 teams', () => {
+        const leaderboardService = new LeaderboardService(MATCHES['head-to-head-with-2-teams']);
+        const leaderboard = leaderboardService.getLeaderboard();
 
-describe("laderboard", () => {
-  let leagueService;
+        expect(leaderboard.length).toBe(4);
 
-  beforeEach(() => {
-    leagueService = new LeagueService();
-  });
+        const expectedResults = [
+            { teamName: 'Brazil', matchesPlayed: 3, goalsFor: 3, goalsAgainst: 3, points: 4 },
+            { teamName: 'Serbia', matchesPlayed: 3, goalsFor: 3, goalsAgainst: 2, points: 4 },
+            { teamName: 'Switzerland', matchesPlayed: 2, goalsFor: 3, goalsAgainst: 4, points: 3 },
+            { teamName: 'Cameroon', matchesPlayed: 2, goalsFor: 1, goalsAgainst: 1, points: 2 }
+        ]
 
-  test('check-leaderboard-teams', async () => {
-    const matches = [
-      {
-        matchDate: Date.now(),
-        stadium: 'Maracanã',
-        homeTeam: 'Brazil',
-        awayTeam: 'France',
-        matchPlayed: true,
-        homeTeamScore: 2,
-        awayTeamScore: 1
-      }
-    ];
-    leagueService.setMatches(matches);
+        for (let i = 0; i < expectedResults.length; i++) {
+            const teamInformation = expectedResults[i]
+            for (let property in teamInformation) {
+                expect(leaderboard[i][property]).toBe(teamInformation[property])
+            }
+        }
+    })
 
-    const leaderboard = leagueService.getLeaderboard();
+    test('Tiebreaker by head-to-head points with 3 teams', () => {
+        const leaderboardService = new LeaderboardService(MATCHES['head-to-head-with-3-teams']);
+        const leaderboard = leaderboardService.getLeaderboard();
 
-    const firstTeam = leaderboard[0];
-    expect(firstTeam.teamName).toBe('Brazil');
-    expect(firstTeam.matchesPlayed).toBe(1);
-    expect(firstTeam.goalsFor).toBe(2);
-    expect(firstTeam.goalsAgainst).toBe(1);
-    expect(firstTeam.points).toBe(3);
+        expect(leaderboard.length).toBe(4);
 
-    const secondTeam = leaderboard[1];
-    expect(secondTeam.teamName).toBe('France');
-    expect(secondTeam.matchesPlayed).toBe(1);
-    expect(secondTeam.goalsFor).toBe(1);
-    expect(secondTeam.goalsAgainst).toBe(2);
-    expect(secondTeam.points).toBe(0);
-  });
+        const expectedResults = [
+            { teamName: 'Cameroon', matchesPlayed: 2, goalsFor: 4, goalsAgainst: 3, points: 4 },
+            { teamName: 'Brazil', matchesPlayed: 2, goalsFor: 4, goalsAgainst: 2, points: 4 },
+            { teamName: 'Serbia', matchesPlayed: 3, goalsFor: 3, goalsAgainst: 2, points: 4 },
+            { teamName: 'Switzerland', matchesPlayed: 3, goalsFor: 4, goalsAgainst: 8, points: 1 },
+        ]
+
+        for (let i = 0; i < expectedResults.length; i++) {
+            const teamInformation = expectedResults[i]
+            for (let property in teamInformation) {
+                expect(leaderboard[i][property]).toBe(teamInformation[property])
+            }
+        }
+    })
+
+    test('Tiebreaker by goal difference', () => {
+        const leaderboardService = new LeaderboardService(MATCHES['tiebreaker-by-goal-difference']);
+        const leaderboard = leaderboardService.getLeaderboard();
+
+        expect(leaderboard.length).toBe(4);
+
+        const expectedResults = [
+            { teamName: 'Serbia', matchesPlayed: 3, goalsFor: 3, goalsAgainst: 1, points: 5 },
+            { teamName: 'Brazil', matchesPlayed: 3, goalsFor: 3, goalsAgainst: 2, points: 5 },
+            { teamName: 'Cameroon', matchesPlayed: 2, goalsFor: 1, goalsAgainst: 1, points: 2 },
+            { teamName: 'Switzerland', matchesPlayed: 2, goalsFor: 2, goalsAgainst: 5, points: 0 },
+        ]
+
+        for (let i = 0; i < expectedResults.length; i++) {
+            const teamInformation = expectedResults[i]
+            for (let property in teamInformation) {
+                expect(leaderboard[i][property]).toBe(teamInformation[property])
+            }
+        }
+    })
+
+    test('Tiebreaker by the number of scored goals', () => {
+        const leaderboardService = new LeaderboardService(MATCHES['tiebreaker-by-the-number-of-scored-goals']);
+        const leaderboard = leaderboardService.getLeaderboard();
+
+        expect(leaderboard.length).toBe(4);
+
+        const expectedResults = [
+            { teamName: 'Brazil', matchesPlayed: 3, goalsFor: 4, goalsAgainst: 2, points: 5 },
+            { teamName: 'Serbia', matchesPlayed: 3, goalsFor: 3, goalsAgainst: 1, points: 5 },
+            { teamName: 'Cameroon', matchesPlayed: 2, goalsFor: 1, goalsAgainst: 1, points: 2 },
+            { teamName: 'Switzerland', matchesPlayed: 2, goalsFor: 2, goalsAgainst: 6, points: 0 },
+        ]
+
+        for (let i = 0; i < expectedResults.length; i++) {
+            const teamInformation = expectedResults[i]
+            for (let property in teamInformation) {
+                expect(leaderboard[i][property]).toBe(teamInformation[property])
+            }
+        }
+    })
+
 });
